8624c58a71d55c9f5d17054c72715cd3ac4478df
Gökhan Bıyıkoğlu <gokhanbyk@Gokhans-MacBook-Air.local>
2023-09-13 16:36:50 +0300

    adding messages framework

--- user/views.py
+++ user/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 def login_view(request):
     if request.user.is_authenticated:
+        messages.info(request, f'{request.user.username}Daha önce login olmuşsun')
         return redirect('home_view')
     
     if request.method == 'POST':
@@ -14,8 +16,11 @@ def login_view(request):
         user = authenticate(request, username = username, password = password)
         if user is not None:
             login(request, user)
+            messages.success(request, f'{request.user.username} Login Oldun')
             return redirect('home_view')
 
     context = dict()
     return render(request, 'user/login.html', context)
 
+def logout_view(request):
+    pass
