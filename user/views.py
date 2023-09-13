e7fe5d426dedd0107bc5e50c799daf1acdcf39c9
Gökhan Bıyıkoğlu <gokhanbyk@Gokhans-MacBook-Air.local>
2023-09-13 16:18:10 +0300

    Finising login

--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -6,6 +7,15 @@ def login_view(request):
     if request.user.is_authenticated:
         return redirect('home_view')
     
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            return redirect('home_view')
+
     context = dict()
     return render(request, 'user/login.html', context)
 
