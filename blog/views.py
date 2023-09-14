54de0d82c146f1743504e897e969c0c7c7634275
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2023-09-14 14:05:05 +0300

    Validators

--- blog/views.py
+++ blog/views.py
@@ -8,18 +8,17 @@ from .models import *
 login_required(login_url='user:login_view')
 def create_blog_post_view(request):
     form = BlogPostModelForm()
-    context = dict(
-        form = form
-    )
-
+   
     if request.method == 'POST':
         form = BlogPostModelForm(request.POST or None, request.FILES or None)
         
         if form.is_valid():
             post = form.save(commit=False)
             post.user = request.user
-            post.save()
+            # post.save()
 
 
-
+    context = dict(
+        form = form
+    )
     return render(request, 'blog/create_blog_post.html', context)
