f1e0f67a48459f7eb9700e175f8a1d4770f55ca0
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2023-09-14 12:25:59 +0300

    editted base_page and adding classes to forms

--- blog/forms.py
+++ blog/forms.py
@@ -3,6 +3,8 @@ from .models import *
 
 class PostModelForm(forms.ModelForm):
     tag = forms.CharField()
+
+    # title = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     class Meta:
         model = Post
         fields = [
@@ -12,3 +14,8 @@ class PostModelForm(forms.ModelForm):
             'category',
             'tag',
         ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs.update({'class': 'form-control'})
+        self.fields['category'].widget.attrs.update({'class': 'form-control'})
