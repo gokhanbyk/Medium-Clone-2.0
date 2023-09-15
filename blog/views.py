a3fa5c90fe1f0d06006c200d3d745a5751466da7
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2023-09-15 14:55:24 +0300

    Made Posts visible for home page and easy-thumbnails usage

--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,7 @@ def create_blog_post_view(request):
             tags = json.loads(tags_json)
             for item in tags:
                 value = item["value"]
-                tag_item, created = Tag.objects.get_or_create(title=value)
+                tag_item, created = Tag.objects.get_or_create(title=value.lower())
                 post.tag.add(tag_item)
             messages.success(request, 'Blog postunuz basariyla kaydedildi..')
             return redirect('home_view')
--- page/views.py
+++ page/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from blog.models import *
 
 # Create your views here.
 
 def home_view(request):
-
-    context = dict()
+    posts = BlogPost.objects.filter(is_active=True).order_by('-created_at')
+    context = dict(
+        posts = posts
+    )
     return render(request, 'page/home_page.html', context)
