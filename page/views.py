4f073cbda29610946fbf549fa90c85364c3074a5
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2023-09-15 17:31:46 +0300

    top_posts

--- page/views.py
+++ page/views.py
@@ -5,10 +5,12 @@ from blog.models import *
 
 def home_view(request):
     posts = BlogPost.objects.filter(is_active=True) # .order_by('-created_at')
+    top_posts = posts.order_by('-view_count')[:6]
     tags = Tag.objects.filter(is_active=True) 
     categories = Category.objects.filter(is_active=True) 
     context = dict(
         posts = posts,
+        top_posts = top_posts,
         tags = tags,
         categories = categories
     )
